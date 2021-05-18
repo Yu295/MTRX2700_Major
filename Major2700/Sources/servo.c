ac73b429f8ba58e1a2b04840c30a87353abeaca4
jlai3637 <80010071+jlai3637@users.noreply.github.com>
2021-05-18 19:43:14 +1000

    Proper prescaling? (to achieve 20 ms period)

--- Major2700/Sources/servo.c
+++ Major2700/Sources/servo.c
@@ -7,12 +7,13 @@ void PWMConfig(void) {
   PWMCLK = (PWMCLK_PCLK5_MASK | PWMCLK_PCLK7_MASK); // select scaled PWM clocks
   PWMPOL = (PWMPOL_PPOL5_MASK | PWMPOL_PPOL7_MASK); // PWM outputs start at HIGH
   PWMCAE = 0;     // PWM output is left-aligned
-  PWMPRCLK = 0x77;   // Period of 2ms so ratio of 48,000
-                  // set clock A and B prescaler to 128 and use SA/SB so prescaled by 256
-                  // use PWMPER = 188
-  PWMPER5 = 188;
-  PWMPER7 = 188;
-  PWMDTY5 = 9;  // left: 0.9/20 * 188, forward: 1.5/20 * 188, right: 2.1/20 * 188
-  PWMDTY7 = 9;  // PWM duty cycle = PWMDTYx / PWMPERx 
+  PWMPRCLK = 0x77; // set clock A and B prescaler to 128    
+  PWMSCLA = 75;    // use SA/SB, and scale these by 256
+  PWMSCLB = 75;    // so total prescaling of 128*75*2 = 19200
+                  // i.e. SA/SB has a period of 0.8 ms               
+  PWMPER5 = 25;   // use PWMPER = 25 so PWM has a period of 20 ms
+  PWMPER7 = 25;
+  PWMDTY5 = 1;  // left: 0.9/20 * 25, forward: 1.5/20 * 25, right: 2.1/20 * 25
+  PWMDTY7 = 1;  // PWM duty cycle = PWMDTYx / PWMPERx 
   PWME = (PWME_PWME5_MASK | PWME_PWME7_MASK);  
 }
