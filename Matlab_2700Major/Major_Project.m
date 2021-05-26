4366ff85b4c5f6cfefe368ad689b8cf696e889bd
Yu295 <ycao8507@uni.sydney.edu.au>
2021-05-27 09:17:25 +1000

    Read a character from serial and check obstacle

--- Matlab_2700Major/Major_Project.m
+++ Matlab_2700Major/Major_Project.m
@@ -1,16 +1,22 @@
 %% Serial
 SerialPort = 'COM5';
+
+% read from serial port to check if there is obstacle detected in front
+%dist_to_obstacle = readLidar(SerialPort);
+
+% send through a flag indicating task finished, start scanning.
+device = (SerialPort,9600);
+write(device, 1, "uint8");
+
+%read from serial the elevation, aimuth, distance and ground distance
 dataMatrix = readSerial(SerialPort);
 
   
 %% Voice Instructions
-
 NET.addAssembly('System.Speech');
 obj = System.Speech.Synthesis.SpeechSynthesizer;
 obj.Volume = 100;
 
-obstacle_detected = 'Obstacle detected in your way. Please stop.';
-stand_still = 'Start scanning. Please stand still.';
 turn_right = 'Please turn right slowly.';
 turn_left = 'Please turn left slowly.';
 move_forward = 'Clear to go forward. Please start walking.';
